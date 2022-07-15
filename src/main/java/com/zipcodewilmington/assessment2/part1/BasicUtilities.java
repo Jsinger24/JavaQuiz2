9b933de07b2286d4cfc3afabf24b4c9b2a3aab64
jd <j.judd.singer@gmail>
2022-07-15 19:59:56 -0400

    ran first test this is first commit

--- src/main/java/com/zipcodewilmington/assessment2/part1/BasicUtilities.java
+++ src/main/java/com/zipcodewilmington/assessment2/part1/BasicUtilities.java
@@ -2,7 +2,12 @@ package com.zipcodewilmington.assessment2.part1;
 
 public class BasicUtilities {
     public Boolean isGreaterThan5(Integer value) {
-        return null;
+        if (value >= 5) {
+            return Boolean.TRUE;
+        } else {
+            return Boolean.FALSE;
+        }
+
     }
 
     public Boolean isLessThan7(Integer value) {
